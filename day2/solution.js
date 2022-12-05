c194b77db856daad7833b9e1a695b760b23711e7
JamesNg <njames15@gmail.com>
2022-12-05 16:05:42 -0500

    day 2 solution

--- day2/solution.js
+++ day2/solution.js
@@ -3,20 +3,11 @@ var fs = require('fs');
 fs.readFile('./input.txt', (err, data) => {
   if (err) throw err;
   let input = ("" + data).split("\n");
-  console.log(input);
+  
+  let total = 0;
+  let actualScore = 0;
 
-  // const shapeGuide = {
-  //   'A': 'rock', 
-  //   'B': 'paper', 
-  //   'C': 'scissor', 
-  //   'X': 'rock', 1 
-  //   'Y': 'paper', 2
-  //   'Z': 'scissor' 3
-
-  // 'Won': 6,
-  // 'Draw': 3,
-  // 'Lost': 0
-  // }
+  //Part 1
   const scoreGuide = {
     "A X": 4,
     "A Y": 8,
@@ -29,11 +20,24 @@ fs.readFile('./input.txt', (err, data) => {
     "C Z": 6,
   };
 
-  let total = 0;
-
+  //Part 2
+  const elfScoreGuide = {
+    "A X": 3,
+    "A Y": 4,
+    "A Z": 8,
+    "B X": 1,
+    "B Y": 5,
+    "B Z": 9,
+    "C X": 2,
+    "C Y": 6,
+    "C Z": 7,
+  };
+  
   for (let round of input) {
     total += scoreGuide[round];
-  };
-
+    actualScore += elfScoreGuide[round];
+  }
+  
   console.log(total)
+  console.log(actualScore)
 });
